33a7c5c4b905823c353773d515c6b11b9f548f24
Samuel Colmenares <locoucla@hotmail.com>
2022-09-06 19:56:10 -0500

    html dinamico

--- js/app.js
+++ js/app.js
@@ -8,12 +8,28 @@ $(document).ready(function() {
                     //console.log('data :>> ', res);
                     let contenido = res.data;
                     console.log('contenido :>> ', contenido);
+                    let cards = '';
 
                     contenido.forEach(usr => {
                         console.log('%cforeach email :>> ', 'background:purple;color:white', usr.email);
+                        const card = `
+                            <div class="card">
+                                <div class="img-card">
+                                    <img src="${usr.avatar}" alt="${usr.first_name}" class="img">
+                                </div>
+                                <div class="info-card">
+                                    <h3>${usr.first_name} ${usr.last_name}</h3>
+                                    <h4>${usr.email}</h4>
+                                    <h4>ID: ${usr.id}</h4>
+                                </div>
+                            </div>`;
+
+                        cards += card;
                     });
 
-                    $.each(contenido, function(i, val) {
+                    // $('.container-ajax').append(cards);
+                    $('.container-ajax').html(cards);
+                    /*$.each(contenido, function(i, val) {
                         console.log('%cforeach jQuery email ' + i + ' :>> ', 'background:green;color:white', val.email);
                     });
 
@@ -22,7 +38,7 @@ $(document).ready(function() {
                             const element = contenido[usr];
                             console.log('%cfor in email :>> ', 'background:blue;color:white', element.email);
                         }
-                    }
+                    }*/
                 },
                 error: function(err) {
                     console.log('err :>> ', err);
